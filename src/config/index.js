ae473b2172983d05e14cb7fba74973bd250d8e23
Wad Werton <wad_werton@mail.ru>
2020-05-06 21:54:47 +0300

    start coctailApp macking

---
+++ src/config/index.js
@@ -0,0 +1,6 @@
+const config = {
+  apiCoctailSearch: "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=",
+  apiCoctailLookup: "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=",
+}
+
+export default config;
